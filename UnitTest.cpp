4707675304a654bbb0339d5d6fad55c8a4636cfb
madiv9820 <themathur901@gmail.com>
2025-01-11 17:18:43 +0530

    Unit Test Cases for the problem

--- UnitTest.cpp
+++ UnitTest.cpp
@@ -11,12 +11,14 @@ private:
 
 public:
     UnitTest() {
-        testcases = {{'annabelle', 2, true},{'leetcode', 3, false},{'true', 4, true}};
+        testcases = {{"annabelle", 2, true}, 
+                     {"leetcode", 3, false}, 
+                     {"true", 4, true}};
     }
     void test() {
         for(int i = 0; i < testcases.size(); ++i) {
             bool result = obj.canConstruct(testcases[i].s, testcases[i].k);
-            cout << "TestCase " <<  i+1 << ": " << ((result == testcases[i].output) ? "true":"false") << endl;
+            cout << "TestCase " <<  i+1 << ": " << ((result == testcases[i].output) ? "passed":"failed") << endl;
         }
     }
 };
